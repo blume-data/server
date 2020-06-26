92dd35c1cce3714b67961ff82d51f137b4831c01
Taranjeet Singh <blumedata@gmail.com>
2020-06-26 12:15:55 +0530

    Update UserController.ts

--- auth/src/Controllers/UserController.ts
+++ auth/src/Controllers/UserController.ts
@@ -27,4 +27,21 @@ export const isUserNameAvailable = async function (req: Req, res: Response) {
     }
 };
 
+export const verifyEmailToken = async function (req: Req, res: Response) {
+    const modelProps = req.query;
+
+    if (modelProps) {
+        /*if (modelProps.userName) {
+            const userExist = await TempUser.findOne({userName: modelProps.userName});
+            console.log('user exist', userExist);
+            if (userExist) {
+                res.status(200).send(false);
+            }
+            else {
+                res.status(200).send(true);
+            }
+        }*/
+    }
+};
+
 
