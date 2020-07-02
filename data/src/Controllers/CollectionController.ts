c3202f43f73102d8789af1e35ffd4bb666e2ef77
Taranjeet Singh <blumedata@gmail.com>
2020-07-02 13:36:01 +0530

    added error messages

--- data/src/Controllers/CollectionController.ts
+++ data/src/Controllers/CollectionController.ts
@@ -5,9 +5,8 @@ import {RANDOM_STRING} from "../util/methods";
 import {CollectionModel} from "../models/Collection";
 import {
     COLLECTION_ALREADY_EXIST,
-    INVALID_BODY_MESSAGE,
-    REQUIRED_PROPERTY_IN_RULES_SHOULD_BE_BOOLEAN,
-    RULE_DOES_NOT_EXIST_IN_RULE
+    INVALID_RULES_MESSAGE,
+    REQUIRED_PROPERTY_IN_RULES_SHOULD_BE_BOOLEAN
 } from "./Messages";
 import {createMethod} from "../services/crudMethods";
 
@@ -27,7 +26,9 @@ export async function createItemSchema(req: Request, res: Response) {
     const randomCollectionName = `_${userName}_${reqBody.name}`;
 
     // Check if there is not other collection with same name and user_id
-    const alreadyExist = await CollectionModel.findOne({ userName: userName, name: reqBody.name }, 'id');
+    const alreadyExist = await CollectionModel.findOne({
+        userName: userName, name: reqBody.name
+    }, 'id');
     if (alreadyExist) {
         throw new BadRequestError(COLLECTION_ALREADY_EXIST);
     }
@@ -37,9 +38,16 @@ export async function createItemSchema(req: Request, res: Response) {
     // Validate Rules
     if (reqBody.rules && typeof reqBody.rules === 'object' && reqBody.rules.length) {
         reqBody.rules.forEach((rule: {type: string, required?: boolean, name: string}) => {
+            // Validate rule type
             if (typeof rule.type === 'string' && SUPPORTED_DATA_TYPES.includes(rule.type)) {
                 // remove all the spaces
                 rule.name = rule.name.split(' ').join('_');
+            }
+            if (rule.required !== true && typeof rule.required !== 'boolean') {
+                isValidBody = false;
+                inValidMessage.push({
+                    message: `${rule.name}: ${REQUIRED_PROPERTY_IN_RULES_SHOULD_BE_BOOLEAN}`
+                });
             }
             else {
                 isValidBody = false;
@@ -52,7 +60,7 @@ export async function createItemSchema(req: Request, res: Response) {
     else {
         isValidBody = false;
         inValidMessage.push({
-            message: `rules is not valid object`,
+            message: INVALID_RULES_MESSAGE,
             field: 'rules'
         })
     }
