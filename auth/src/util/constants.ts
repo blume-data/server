3bc24eeacd9fac7bc154b1875dc0b1c7d82b50fe
Taranjeet Singh <blumedata@gmail.com>
2020-07-21 19:29:24 +0530

    Update constants.ts

--- auth/src/util/constants.ts
+++ auth/src/util/constants.ts
@@ -1,8 +1,6 @@
-import {PRODUCTION_ENV} from "@ranjodhbirkaur/common";
-
 export const stringLimitOptions = { min: 1, max: 40 };
 export const stringLimitOptionErrorMessage = (field: string) => field+' must be present';
-export const rootUrl = `/auth/${PRODUCTION_ENV}`;
+export const rootUrl = `/auth`;
 export const clusterUrl = '/events';
 export const okayStatus = 202;
 export const errorStatus = 400;
