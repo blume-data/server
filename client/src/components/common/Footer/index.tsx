daddd93327454369446849416925a2eb19965dd3
Taranjeet Singh <blumedata@gmail.com>
2020-08-30 13:53:08 +0530

    Removed Client

--- client/src/components/common/Footer/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './footer.scss';
-export default () => {
-    return (
-        <div className="appFooter" />
-    );
-}
--- client/src/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { newsReducer } from './components/App/reducers';
-import {combineReducers} from "redux";
-
-export const rootReducer = combineReducers({
-    newsData: newsReducer
-});
-
-export type NewsStateType = ReturnType<typeof newsReducer>;
-export type RootState = ReturnType<typeof rootReducer>
