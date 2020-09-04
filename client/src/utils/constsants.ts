8f7ca04584b55239af9c1ddb7c2fc0da2a9ba339
Taranjeet Singh <blumedata@gmail.com>
2020-09-04 18:12:12 +0530

    deleted some files

--- client/src/utils/constsants.ts
+++
@@ -1,3 +0,0 @@
-export const DEVELOPMENT_ENV = 'development';
-export const PRODUCTION_ENV = 'production';
-export const EXTERNAL_LINK = 'https://news.ycombinator.com/';
--- client/src/utils/router-urls.ts
+++
@@ -1,3 +0,0 @@
-export const loginRouterPath = '/auth/log-in';
-export const getStartedRouterPath = '/auth/get-started';
-export const logoutRouterPath = '/auth/log-out';
