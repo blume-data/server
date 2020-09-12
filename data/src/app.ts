35e78a6e28ca4a092f272f36706550826c6b7f7f
Taranjeet Singh <blumedata@gmail.com>
2020-09-12 10:55:09 +0530

    saving

--- data/src/app.ts
+++ data/src/app.ts
@@ -1,38 +1,17 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
+import {serverApp} from "@ranjodhbirkaur/common";
 import { errorHandler, NotFoundError } from '@ranjodhbirkaur/common';
-import cors from 'cors';
-import compression from 'compression';
-
 import {CollectionRoutes} from "./routes/collection-routes";
 import {StoreRoutes} from "./routes/store";
 import {RoleRoutes} from "./routes/roles-routes";
 
+serverApp.use(RoleRoutes);
+serverApp.use(CollectionRoutes);
+serverApp.use(StoreRoutes);
 
-const app = express();
-
-app.use(cors());
-app.set('trust proxy', true);
-app.use(compression());
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: false
-  })
-);
-app.options('*', cors());
-
-app.use(RoleRoutes);
-app.use(CollectionRoutes);
-app.use(StoreRoutes);
-
-app.all('*', async (req, res) => {
+serverApp.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
-app.use(errorHandler);
+serverApp.use(errorHandler);
 
-export { app };
+export { serverApp as app };
