d1d23244e08cefdefa814e2b842540e9a808e4cb
Taranjeet Singh <blumedata@gmail.com>
2020-09-13 11:37:00 +0530

    Update store.ts

--- database/src/routes/store.ts
+++ database/src/routes/store.ts
@@ -1,5 +1,6 @@
 import express, {Response, Request} from "express";
 import {RanjodhbirModel} from "../ranjodhbirDb/model";
+//import axios from 'axios';
 
 const route = express.Router();
 
@@ -7,9 +8,18 @@ route.post('/store', async (req: Request, res: Response) => {
     const db = new RanjodhbirModel('store-data', [{name: 'age', type: "number"}]);
     await db.createSchema();
 
-    await db.storeData({age: 54});
+    for(let i = 1; i<25000; i++) {
+        //await db.storeData({age: 3242432432434324});
+    }
     const data = await db.readData();
     res.send(data);
 });
+
+/*route.post('/store/test', async (req: Request, res: Response) => {
+    console.log('hello testing')
+    const response = await axios.post('http://database-srv:3000/store');
+    console.log(response.data);
+    res.send(response.data);
+});*/
 
 export { route as StoreRoutes };
