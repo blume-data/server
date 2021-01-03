f353037c59269c53a4a6431f2cb4395c57a3e79b
Taranjeet Singh <blumedata@gmail.com>
2021-01-03 14:34:36 +0530

    saving

--- assets/src/config.ts
+++ assets/src/config.ts
@@ -1,4 +1,4 @@
-export const accessKeyId = 'AKIA3WAXJDKJ6EPRBF5V';
+export const accessKeyId = 'AKIA3WAXJDKJTN2I7WYU';
 export const AwsRegionCode = "us-east-1";
 export const AwsBucketName = "ranjod-assets";
 export const AwsImageRootUrl = `https://s3.${AwsRegionCode}.amazonaws.com/${AwsBucketName}/`;
