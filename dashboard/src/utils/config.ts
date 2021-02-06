7fa20a7983631f41ce441ec954b66dea49443197
Taranjeet Singh <blumedata@gmail.com>
2021-02-06 15:08:17 +0530

    added api routes

--- dashboard/src/utils/config.ts
+++ dashboard/src/utils/config.ts
@@ -1,1 +1,1 @@
-export const ROOT_URL = process.env.NODE_ENV === 'development' ? `https://dev.ranjod.com` : 'https://ranjod.com';
+export const ROOT_URL = process.env.NODE_ENV === 'development' ? `https://dev.ranjod.com` : 'https://api.ranjod.com';
