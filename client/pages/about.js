122add5cdf08cf874c28eb1fe9253c20d32b757d
Taranjeet Singh <67357058+blumedata@users.noreply.github.com>
2021-03-07 08:30:36 +0530

    saving

---
+++ client/pages/about.js
@@ -0,0 +1,10 @@
+import styles from '../styles/About.module.scss'
+
+export default function About() {
+    return (
+        <div className={styles.aboutContainer}>
+            ABout me
+            <button className="button">Button</button>
+        </div>
+    );
+}
