ac0ef23b0337a9fd4955fdeaac91af5ffda5386c
Taranjeet Singh <67357058+blumedata@users.noreply.github.com>
2021-05-09 10:46:37 +0530

    updated urls in dashboard

--- dashboard/src/utils/constants/index.ts
+++ dashboard/src/utils/constants/index.ts
@@ -5,7 +5,7 @@ export const LOCAL_STORAGE_LANGUAGE = 'LOCAL_STORAGE_LANGUAGE';
 export const LOCAL_STORAGE_SELECTED_APPLICATION_NAME = 'LOCAL_STORAGE_SELECTED_APPLICATION_NAME';
 
 export const AUTH_ROUTES = {
-    authRootUrl: "/auth-api",
+    authRootUrl: "/api/auth",
     currentUser: "current-user",
     emailVerification: "email-verification",
     logIn: "log-in",
@@ -15,20 +15,20 @@ export const AUTH_ROUTES = {
 }
 
 export const DATA_ROUTES = {
-    ApplicationNameUrl: "/data-api/:clientUserName/application-space/:applicationName?",
-    CollectionUrl: "/data-api/:env/:clientUserName/:applicationName/models",
-    GetCollectionNamesUrl: "/data-api/:env/:clientUserName/:applicationName/get-models",
-    GetEntriesUrl: "/data-api/:env/:language/:clientUserName/:applicationName/entry/:modelName/get-entries",
-    RoleUrl: "/data-api/:env/:userName/role/:roleName?",
-    StoreUrl: "/data-api/:env/:language/:clientUserName/:applicationName/entry/:modelName",
+    ApplicationNameUrl: "/api/data/:clientUserName/application-space/:applicationName?",
+    CollectionUrl: "/api/data/:env/:clientUserName/:applicationName/models",
+    GetCollectionNamesUrl: "/api/data/:env/:clientUserName/:applicationName/get-models",
+    GetEntriesUrl: "/api/data/:env/:language/:clientUserName/:applicationName/entry/:modelName/get-entries",
+    RoleUrl: "/api/data/:env/:userName/role/:roleName?",
+    StoreUrl: "/api/data/:env/:language/:clientUserName/:applicationName/entry/:modelName",
 }
 
 export const ASSETS_ROUTE = {
-    AssetsGetAssetsDataUrl: "/assets-api/:clientUserName/asset",
-    authAssets: "/assets-api/:clientUserName/secure-assets",
-    getAsset: "/assets-api/:clientUserName/get/asset",
-    getAssets: "/assets-api/:clientUserName",
-    createTempRecord: "/assets-api/:clientUserName/create-temp-record",
-    verifyTempRecord: "/assets-api/:clientUserName/verify-temp-record",
-    verifyAssets: "/assets-api/:clientUserName/verify-url"
+    AssetsGetAssetsDataUrl: "/api/assets/:clientUserName/asset",
+    authAssets: "/api/assets/:clientUserName/secure-assets",
+    getAsset: "/api/assets/:clientUserName/get/asset",
+    getAssets: "/api/assets/:clientUserName",
+    createTempRecord: "/api/assets/:clientUserName/create-temp-record",
+    verifyTempRecord: "/api/assets/:clientUserName/verify-temp-record",
+    verifyAssets: "/api/assets/:clientUserName/verify-url"
 }
