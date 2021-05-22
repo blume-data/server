9b981f855a5c600987727f15fadf9e533ed1e8fc
Taranjeet Singh <67357058+taranjeetsingh0@users.noreply.github.com>
2021-05-22 22:13:30 +0530

    Added Env In application space

--- api/src/modules/auth/Controllers/EnvController.ts
+++ api/src/modules/auth/Controllers/EnvController.ts
@@ -1,5 +1,7 @@
+import { trimCharactersAndNumbers } from '@ranjodhbirkaur/constants';
 import { Request, Response } from 'express';
 import { ApplicationSpaceModel } from '../../../db-models/ApplicationSpace';
+import { sendSingleError } from '../../../util/common-module';
 import { sendOkayResponse } from '../../../util/methods';
 
 export async function CreateEnv(req: Request, res: Response) {
@@ -7,12 +9,31 @@ export async function CreateEnv(req: Request, res: Response) {
 
     const clientUserName = req.params.clientUserName;
     const applicationName = req.params.applicationName;
+    const name = req.body.name;
 
-    const ApplicationNameEntry = await ApplicationSpaceModel.findOneAndUpdate({
-        clientUserId: req.currentUser.id,
-        
-    })
+    const ApplicationNameEntry = await ApplicationSpaceModel.findOne({
+        clientUserName,
+        name: applicationName
+    }, ['env']);
 
-    sendOkayResponse(res, {status: true});
+    if(ApplicationNameEntry && ApplicationNameEntry.env && Array.isArray(ApplicationNameEntry.env)) {
+        const exist = ApplicationNameEntry.env.find(item => item === name);
+        if(exist) {
+            return sendSingleError(res, 'Env already exist', 'name');
+        }
+        else {
+            ApplicationNameEntry.env.push(trimCharactersAndNumbers(name));
+            // update the applicationSpace
+            await ApplicationSpaceModel.findOneAndUpdate({
+                clientUserName, name: applicationName
+            }, {
+                env: ApplicationNameEntry.env
+            });
+        }
+
+    }
+    console.log('Application', ApplicationNameEntry)
+
+    sendOkayResponse(res, {status: 'done'});
    
 }
--- api/src/modules/auth/routes/env-route.ts
+++ api/src/modules/auth/routes/env-route.ts
@@ -11,7 +11,7 @@ const router = Router();
 
 router.post(EnvUrl, checkAuth,
 
-    body('env')
+    body('name')
     .trim()
     .isLength(stringLimitOptions)
     .withMessage(stringLimitOptionErrorMessage(ENV)),
