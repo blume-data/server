facec21ea32ec1bc4ca7dfffbd5a861b3e1b7165
Taranjeet Singh <67357058+taranjeetsingh0@users.noreply.github.com>
2021-05-24 01:06:01 +0530

    Update EnvController.ts

--- api/src/modules/auth/Controllers/EnvController.ts
+++ api/src/modules/auth/Controllers/EnvController.ts
@@ -1,6 +1,7 @@
-import { trimCharactersAndNumbers } from '@ranjodhbirkaur/constants';
+import { ID, trimCharactersAndNumbers } from '@ranjodhbirkaur/constants';
 import { Request, Response } from 'express';
 import { ApplicationSpaceModel } from '../../../db-models/ApplicationSpace';
+import { EnvModel } from '../../../db-models/Env';
 import { sendSingleError } from '../../../util/common-module';
 import { sendOkayResponse } from '../../../util/methods';
 
@@ -11,23 +12,33 @@ export async function CreateEnv(req: Request, res: Response) {
     const name = req.body.name;
     const description = req.body.description || '';
 
-    const ApplicationNameEntry = await ApplicationSpaceModel.findOne({
+    const ApplicationNameEntry: any = await ApplicationSpaceModel.findOne({
         clientUserName,
         name: applicationName
-    }, ['env']);
+    }, ['env']).populate('env');
 
     
 
     if(ApplicationNameEntry && ApplicationNameEntry.env && Array.isArray(ApplicationNameEntry.env)) {
         
-        const exist = ApplicationNameEntry.env.find(item => item.name === trimCharactersAndNumbers(name));
+        const exist = ApplicationNameEntry.env.find((item: any) => item.name === trimCharactersAndNumbers(name));
         if(exist) {
             return sendSingleError(res, 'Env already exist', 'name');
         }
         else {
-            ApplicationNameEntry.env.push({
-                name: trimCharactersAndNumbers(name), description, order: ApplicationNameEntry.env.length + 1
+            // create a new env
+            const newEnv = EnvModel.build({
+                name: trimCharactersAndNumbers(name),
+                description,
+                order: ApplicationNameEntry.env.length + 1,
+                isPublic: true,
+                supportedDomains: [''],
+                createdBy: req.currentUser[ID],
+                updatedBy: req.currentUser[ID],
             });
+            await newEnv.save();
+            ApplicationNameEntry.env.push(newEnv[ID]);
+            
             // update the applicationSpace
             await ApplicationSpaceModel.findOneAndUpdate({
                 clientUserName, name: applicationName
