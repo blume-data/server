f853ccc268d8525b9495166bfcbbc2b108319ed4
Taranjeet Singh <taranjeet.singh3009@gmail.com>
2022-01-24 21:17:07 +0530

    fixed one bug

--- dashboard/src/components/App/index.tsx
+++ dashboard/src/components/App/index.tsx
@@ -4,7 +4,14 @@ import React from 'react';
 export default () => {
     return (
         <div className="App">
-            <h1>App</h1>
+            <h1>Welcome to Blumne</h1>
+            <h3>Create a free acount for:</h3>
+            <ul>
+                <li>Content management system</li>
+                <li>Easy addition of validations</li>
+                <li>Simple usage of apis</li>
+                
+            </ul>
         </div>
     );
 };
