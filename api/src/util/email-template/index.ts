9b5aae54b8259666f6e0f1d9b7ee77cef931046d
singh-taranjeet <93113908+singh-taranjeet@users.noreply.github.com>
2022-01-24 18:13:33 +0530

    Main (#181)
    * fixed dashbaord
    
    * fix
    
    * added validation for email
    
    * fixed email
    
    * Update package.json
    
    * email fix
    
    * Update index.ts
    
    * Update email.ts
    
    * emal fix

--- api/src/util/email-template/index.ts
+++ api/src/util/email-template/index.ts
@@ -108,7 +108,7 @@ export function emailTemplate(url:string, name: string, token: string) {
         <table border="0" cellpadding="0" cellspacing="0" width="100%">
             <!-- LOGO -->
             <tr>
-                <td bgcolor="#FFA73B" align="center">
+                <td bgcolor="#3d44c3" align="center">
                     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                         <tr>
                             <td align="center" valign="top" style="padding: 40px 10px 40px 10px;"> </td>
@@ -117,7 +117,7 @@ export function emailTemplate(url:string, name: string, token: string) {
                 </td>
             </tr>
             <tr>
-                <td bgcolor="#FFA73B" align="center" style="padding: 0px 10px 0px 10px;">
+                <td bgcolor="#3d44c3" align="center" style="padding: 0px 10px 0px 10px;">
                     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                         <tr>
                             <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
@@ -147,7 +147,7 @@ export function emailTemplate(url:string, name: string, token: string) {
                                         <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 60px 30px;">
                                             <table border="0" cellspacing="0" cellpadding="0">
                                                 <tr>
-                                                    <td align="center" style="border-radius: 3px;" bgcolor="#FFA73B"><a href="${url}" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #FFA73B; display: inline-block;">Confirm Account</a></td>
+                                                    <td align="center" style="border-radius: 3px;" bgcolor="#3d44c3"><a href="${url}" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #3d44c3; display: inline-block;">Confirm Account</a></td>
                                                 </tr>
                                             </table>
                                         </td>
@@ -163,7 +163,7 @@ export function emailTemplate(url:string, name: string, token: string) {
                         </tr> <!-- COPY -->
                         <tr>
                             <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 20px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                                <p style="margin: 0;"><a href="#" target="_blank" style="color: #FFA73B;">${url}</a></p>
+                                <p style="margin: 0;"><a href="#" target="_blank" style="color: #3d44c3;">${url}</a></p>
                             </td>
                         </tr>
                         <tr>
