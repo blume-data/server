a785ccc72fcb0a6a41121d28d7b9c645005646f5
singh-taranjeet <93113908+singh-taranjeet@users.noreply.github.com>
2023-05-06 12:30:16 +0530

    removed material ui button

--- dashboard/src/components/common/Button/index.tsx
+++ dashboard/src/components/common/Button/index.tsx
@@ -29,6 +29,6 @@ export const Button = (props: {
   }
 
   return (
-    <button className={`rounded-full ${className}`} onClick={onButtonClick}>{children}</button>
+    <button className={`rounded-full bg-blue-600 py-2 px-4 text-white ${className}`} onClick={onButtonClick}>{name ? name : children}</button>
   );
 };
