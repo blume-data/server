2d87ea076cd350546e7253fc6e2bd044934fef48
singh-taranjeet <93113908+singh-taranjeet@users.noreply.github.com>
2023-05-06 11:08:57 +0530

    removed-story-book

--- dashboard/src/index.tsx
+++ dashboard/src/index.tsx
@@ -6,7 +6,7 @@ import { rootReducer, RouteAddressStateType } from "./rootReducer";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import React from "react";
+import React, { Profiler } from "react";
 import reportWebVitals from "./reportWebVitals";
 import { paletteColor } from "./utils/constants";
 import "./index.scss";
@@ -37,15 +37,26 @@ const theme = createMuiTheme({
   palette: paletteColor,
 });
 
+const onRender = (id: any, phase:any, actualDuration:any, baseDuration:any, startTime:any, commitTime:any) => {
+  // Aggregate or log render timings...
+  const logs = {
+    id, phase, actualDuration, baseDuration, startTime, commitTime
+  };
+
+  console.log("Log", logs);
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <MuiThemeProvider theme={theme}>
-        <BrowserRouter>
-          <RouterComponent />
-        </BrowserRouter>
-      </MuiThemeProvider>
-    </Provider>
+    <Profiler id="profiler-app" onRender={onRender}>
+      <Provider store={store}>
+        <MuiThemeProvider theme={theme}>
+          <BrowserRouter>
+            <RouterComponent />
+          </BrowserRouter>
+        </MuiThemeProvider>
+      </Provider>
+      </Profiler>
   </React.StrictMode>,
   document.getElementById("root")
 );
